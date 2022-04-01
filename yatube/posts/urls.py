41913cafac74d857ab81738afec0f106c3479a6a
Danila Karzhev <in-carne@ya.ru>
2022-04-01 05:07:30 +0300

    Непонятки с путём

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # Главная страница
     path('', views.index),
     # Страница со списком мороженого
-    path('group/<str:any_slug>/', views.group_posts),
+    path('posts/<str:any_slug>/', views.group_posts),
     # Страница с информацией об одном сорте мороженого;
     # в качестве параметра ожидает целое положительное число или 0
     # path('posts/<pk>/', views.posts_detail),
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 # def index(request, user):
 #     return HttpResponse(f'<h1> Сайт Ани Каржевой </h1> <p> Привет, {user}! Я фотограф!</p>')
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, any_slug):
     return HttpResponse(f'а у нас {any_slug}')
-
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
 # def posts_detail(request, pk):
